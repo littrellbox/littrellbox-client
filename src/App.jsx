bd5ea6220270f0786bbd888f63c42cefdb5f6b17
william341 <wwokwilliam@gmail.com>
2020-01-28 22:20:06 -0700

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import socketClient from 'socket.io-client'
+import SocketContext from './contexts/socketContext'
+
+import './css/App.css';
+
+import AppLoading from './misc/AppLoading'
+
+const uri = 'http://localhost:3001'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isConnected: false,
+    }
+
+    //create the socket
+    this.socket = socketClient(uri)
+
+    this.onConnect = this.onConnect.bind(this)
+    this.onDisconnect = this.onConnect.bind(this)
+  }
+
+  componentDidMount() {
+    //hook up connection functions
+    this.socket.on("connect", this.onConnect);
+    this.socket.on("disconnect", this.onDisconnect);
+  }
+
+  onConnect() {
+    this.setState({
+      isConnected: true
+    })
+  }
+
+  onDisconnect() {
+    this.setState({
+      isConnected: false
+    })
+  }
+
+  render() {
+    return (
+      <SocketContext.Provider value={this.socket}> {/* We need this to pass the socket down to child components */}
+        <div className="App">
+          {!this.state.isConnected && <AppLoading/>}
+        </div>
+      </SocketContext.Provider>
+    )
+  }
+}
+
+export default App;
---
+++ src/misc/AppLoading.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './css/AppLoading.css'
+
+class AppLoading extends React.Component {
+  render() {
+    return (
+      <div className="AppLoading">
+        Connecting to server
+      </div>
+    )
+  }
+}
+
+export default AppLoading
